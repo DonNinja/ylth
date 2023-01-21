222868e3f69c8c239e7b1bdcd1a01b98c2f97a11
DonNinja <yngvileo@gmail.com>
2023-01-21 15:59:39 +0000

    Set a new function to easily add new project

---
+++ src/components/links.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Title } from "./functions";
+
+const Links = () => {
+    return (
+        <>
+            {Title(`Links`) }
+            <h1>Links</h1>
+        </>
+    );
+}
+
+export default Links;
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,12 +1,21 @@
-import { Title } from "./functions";
+import { Project, Title } from "./functions";
+import '../styles/projects.css';
 
-const Projects = () => {
-    return (
-        <>
-            {Title("My projects")}
-            <p>These will be my projects</p>
-        </>
-    );
-}
+const Projects = () => (
+    <>
+        {Title("My projects")}
+        <div className="projectsContainer">
+            <h1 className="sectionTitle">Projects</h1>
+            {Project(
+                "Mana Rumble",
+                `<Insert description here>`,
+                "",
+                ["Unity", "C#"],
+                "https://img.itch.zone/aW1nLzg5MTM4NzMucG5n/original/bGatto.png",
+                "Test image"
+            )}
+        </div>
+    </>
+);
 
 export default Projects;
