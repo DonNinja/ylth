2693fbde58b25add4269012c0bb3a2c57366b3f8
DonNinja <yngvileo@gmail.com>
2023-01-23 16:58:30 +0000

    Moved project function into projects.tsx

--- src/components/functions.tsx
+++ src/components/functions.tsx
@@ -5,21 +5,3 @@ export function Title(title: string) {
         document.title = title;
     })
 }
-
-export function Project(title: string, description: string, link: string, languages: string[], image?: string, imageDescription?: string) {
-    return (
-        <>
-            <h2> {title} </h2>
-            <div className="projectDetails">
-                <div className="projectDescription">
-                    {description}
-                </div>
-                {
-                    // This works as an if statement since 'true && expression' evaluates to expression which is useful
-                    image !== "" &&
-                    <img src={image} alt={imageDescription} className="projectLogo" />
-                }
-            </div>
-        </>
-    );
-}
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,5 +1,27 @@
-import { Project, Title } from "./functions";
+import { Title } from "./functions";
 import '../styles/projects.css';
+import { Link } from "react-router-dom";
+
+function Project(title: string, description: string, link: string, languages: string[], image?: string, imageDescription?: string) {
+    return (
+        <>
+            <h2> {title} </h2>
+            <div className="projectDetails">
+                <div className="projectDescription">
+                    <p>
+                        {description}
+                    </p>
+                    <p className=""><Link to={link} replace={true} target="_blank">View the project</Link></p>
+                </div>
+                {
+                    // This works as an if statement since 'true && expression' evaluates to expression which is useful
+                    image !== "" &&
+                    <img src={image} alt={imageDescription} className="projectLogo" />
+                }
+            </div>
+        </>
+    );
+}
 
 const Projects = () => (
     <>
@@ -9,7 +31,7 @@ const Projects = () => (
             {Project(
                 "Mana Rumble",
                 `<Insert description here>`,
-                "",
+                "https://yngvi19.itch.io/mana-rumble",
                 ["Unity", "C#"],
                 "https://img.itch.zone/aW1nLzg5MTM4NzMucG5n/original/bGatto.png",
                 "Test image"
