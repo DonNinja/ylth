ff278256099554fd3e6357125ff65053f631326c
DonNinja <yngvileo@gmail.com>
2023-01-23 19:30:03 +0000

    Made some changes to the Project function (check desc.)
    - Added project link as a
    - Added a languages list to the bottom of the description
    - Changed the project description to have HTML injection

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,17 +1,15 @@
 import { Title } from "./functions";
 import '../styles/projects.css';
-import { Link } from "react-router-dom";
 
-function Project(title: string, description: string, link: string, languages: string[], image?: string, imageDescription?: string) {
+function Project(title: string, description: string, link: string, languages: string, image?: string, imageDescription?: string) {
     return (
         <>
             <h2> {title} </h2>
             <div className="projectDetails">
-                <div className="projectDescription">
-                    <p>
-                        {description}
-                    </p>
-                    <p className=""><Link to={link} replace={true} target="_blank">View the project</Link></p>
+                <div className="projectText">
+                    <p className="projectDescription" dangerouslySetInnerHTML={{ __html: description }}></p>
+                    <p className="projectLink"><a href={link} rel="noreferrer" target="_blank">View the project</a></p>
+                    <p className="projectLanguages"><i>Languages and/or tools used</i>: {languages}</p>
                 </div>
                 {
                     // This works as an if statement since 'true && expression' evaluates to expression which is useful
@@ -30,9 +28,9 @@ const Projects = () => (
             <h1 className="sectionTitle">Projects</h1>
             {Project(
                 "Mana Rumble",
-                `<Insert description here>`,
+                `<p>Insert description here</p> This should be second line`,
                 "https://yngvi19.itch.io/mana-rumble",
-                ["Unity", "C#"],
+                "Unity, C#",
                 "https://img.itch.zone/aW1nLzg5MTM4NzMucG5n/original/bGatto.png",
                 "Test image"
             )}
