df29e3ea411181808e34533f97d27e535befe66d
DonNinja <yngvileo@gmail.com>
2023-02-08 16:00:48 +0000

    Added to projects page

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,6 +1,7 @@
 import { Title } from "./functions";
 import '../styles/projects.css';
 import ManaRumble from "../images/Projects/ManaRumble.png";
+import YCP from "../images/Projects/YCP.png";
 
 function Project(title: string, description: string, link: string, languages: string, image?: string, imageDescription?: string) {
     return (
@@ -29,11 +30,27 @@ const Projects = () => (
             <h1 className="sectionTitle">Projects</h1>
             {Project(
                 "Mana Rumble",
-                `<p>Insert description here</p>`,
+                `
+                    Mana Rumble was our final project during our Computer Science studies in Reykjavík University. It was developed using Unity while utilising Rewired so that two players can use separate controllers on the same machine.
+                <p>
+                    Mana Rumble is a team based PVP fantasy sports game, utilising a simplified version of handball while allowing the player to control any of 3 characters, each with an ability based on fantasy classes found in fantasy roleplaying games.
+                </p>`,
                 "https://yngvi19.itch.io/mana-rumble",
                 "Unity, C#",
                 ManaRumble,
                 "Mana Rumble logo"
+            )}
+            {Project(
+                "YouTube Chapter Pauser",
+                `   YouTube Chapter Pauser is a personal project of mine. It was thought up as I was watching long YouTube videos and realising I was going to go do something but not wanting to pause there and then. I then realised that the video I was watching had chapters and it would be cool if I could stop the video when the next chapter started.
+                
+                <p>
+                    This is a chrome extension that sets a YouTube video to stop when it reaches the next chapter as set by the video uploader.
+                </p>`,
+                "https://chrome.google.com/webstore/detail/youtube-chapter-pauser/igaciglbekobidklgklaejjkoefpcdgf",
+                "Javascript, CSS",
+                YCP,
+                "YouTube Chapter Pauser icon"
             )}
         </div>
     </>
