47f6aa0ae67f623b3ca885e265f4b2ec8cd426c0
DonNinja <yngvileo@gmail.com>
2023-02-27 18:08:16 +0000

    Update links.tsx

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -21,10 +21,8 @@ const Links = () => {
             <div className="pageContainer">
                 <h1 className="title">Links</h1>
                 <div className="linksContainer">
-                    {Link("LinkedIn", "My LinkedIn page", "", "https://cdn-icons-png.flaticon.com/128/174/174857.png", "Linkedin logo")}
-                    {Link("LinkedIn", "My LinkedIn page", "", "https://cdn-icons-png.flaticon.com/128/174/174857.png", "Linkedin logo")}
-                    {Link("GitHub", "Github Page", "", "https://cdn-icons-png.flaticon.com/512/733/733553.png", "GitHub logo")}
-                    {Link("GitHub", "Github Page", "", "https://cdn-icons-png.flaticon.com/512/733/733553.png", "GitHub logo")}
+                    {Link("LinkedIn", "My LinkedIn page", "https://www.linkedin.com/in/yngvi-leó-þráinsson-533630239/", "https://cdn-icons-png.flaticon.com/128/174/174857.png", "Linkedin logo")}
+                    {Link("GitHub", "Github Page", "https://github.com/DonNinja", "https://cdn-icons-png.flaticon.com/512/733/733553.png", "GitHub logo")}
                 </div>
             </div>
         </>
