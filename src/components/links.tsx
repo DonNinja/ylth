8cd7ea9066771e88129ce39e1783725b2c641d1a
DonNinja <yngvileo@gmail.com>
2023-02-27 16:30:41 +0000

    Working on links page

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import "../styles/links.css"
 import { Title } from "./functions";
 
-function Link(title: string, shortDescription: string, link: string, image: string) {
+function Link(title: string, shortDescription: string, link: string, image: string, imageAlt: string) {
     return (
-        <div className="link">
+        <a href={link} className="link">
             <h3>{title}</h3>
             <div className="linkDetails">
-                This is a test
+                {shortDescription}
+                <img className="icon" src={image} alt={imageAlt} />
             </div>
-        </div>
+        </a>
     );
 }
 
@@ -20,8 +21,10 @@ const Links = () => {
             <div className="pageContainer">
                 <h1 className="title">Links</h1>
                 <div className="linksContainer">
-                    {Link("LinkedIn", "My LinkedIn page", "", "")}
-                    
+                    {Link("LinkedIn", "My LinkedIn page", "", "https://cdn-icons-png.flaticon.com/128/174/174857.png", "Linkedin logo")}
+                    {Link("LinkedIn", "My LinkedIn page", "", "https://cdn-icons-png.flaticon.com/128/174/174857.png", "Linkedin logo")}
+                    {Link("GitHub", "Github Page", "", "https://cdn-icons-png.flaticon.com/512/733/733553.png", "GitHub logo")}
+                    {Link("GitHub", "Github Page", "", "https://cdn-icons-png.flaticon.com/512/733/733553.png", "GitHub logo")}
                 </div>
             </div>
         </>
