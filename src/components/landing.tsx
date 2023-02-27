67d7b574c04eb887a7869ffd47fb5c9eb0618139
DonNinja <yngvileo@gmail.com>
2023-02-27 19:18:45 +0000

    Fix landing links

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -12,7 +12,7 @@ const Landing = () => (
             <h2><center>Disclaimer: This website has not been finished yet, and some sites have little to no content.</center></h2>
             <div className="content">
                 <p>
-                    Welcome to my personal website! I am Yngvi Leó Þráinsson, an Icelander who graduated from Reykjavík University with a BSc. in Computer Science. For more info about me check my <Link to="/about">About Me</Link> page, and my past projects will be found within the <Link to="/projects">Projects</Link> page.
+                    Welcome to my personal website! I am Yngvi Leó Þráinsson, an Icelander who graduated from Reykjavík University with a BSc. in Computer Science. For more info about me check my <Link to="/ylth/about">About Me</Link> page, and my past projects will be found within the <Link to="/ylth/projects">Projects</Link> page.
                 </p>
                 {/* <p>
                     <center>
