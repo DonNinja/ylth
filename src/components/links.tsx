91dcf91b322933eeecf5e8f9d4e79f0cf9cdbb1a
DonNinja <yngvileo@gmail.com>
2023-02-27 18:23:32 +0000

    Added links to links page

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import "../styles/links.css"
 import { Title } from "./functions";
 
-function Link(title: string, shortDescription: string, link: string, image: string, imageAlt: string) {
+function Link(title: string, shortDescription: string, link: string, image?: string, imageAlt?: string) {
     return (
-        <a href={link} className="link">
+        <a href={link} target="_blank" className="link" rel="noreferrer">
             <h3>{title}</h3>
             <div className="linkDetails">
                 {shortDescription}
-                <img className="icon" src={image} alt={imageAlt} />
+                <br />
+                {
+                    image ?
+                        <img className="icon" src={image} alt={imageAlt} /> :
+                        <></>
+                }
             </div>
         </a>
     );
@@ -23,6 +28,8 @@ const Links = () => {
                 <div className="linksContainer">
                     {Link("LinkedIn", "My LinkedIn page", "https://www.linkedin.com/in/yngvi-leó-þráinsson-533630239/", "https://cdn-icons-png.flaticon.com/128/174/174857.png", "Linkedin logo")}
                     {Link("GitHub", "Github Page", "https://github.com/DonNinja", "https://cdn-icons-png.flaticon.com/512/733/733553.png", "GitHub logo")}
+                    {Link("Itch.io", "My personal itch.io page", "https://donninja.itch.io")}
+                    {Link("Contact me", "Send me an email", "mailto:www.yngvilth@gmail.com")}
                 </div>
             </div>
         </>
