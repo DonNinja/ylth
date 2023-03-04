5ad1c5cce56b203824bf34c72e16f93bdcae4ce0
DonNinja <yngvileo@gmail.com>
2023-03-04 14:08:16 +0000

    Made some colour style updates

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -27,8 +27,10 @@ const Links = () => {
                 <h1 className="title">Links</h1>
                 <div className="linksContainer">
                     {Link("LinkedIn", "My LinkedIn page", "https://www.linkedin.com/in/yngvi-leó-þráinsson-533630239/", "https://cdn-icons-png.flaticon.com/128/174/174857.png", "Linkedin logo")}
-                    {Link("GitHub", "Github Page", "https://github.com/DonNinja", "https://cdn-icons-png.flaticon.com/512/733/733553.png", "GitHub logo")}
+                    {Link("GitHub", "My personal Github page", "https://github.com/DonNinja", "https://cdn-icons-png.flaticon.com/512/733/733553.png", "GitHub logo")}
                     {Link("Itch.io", "My personal itch.io page", "https://donninja.itch.io")}
+
+
                     {Link("Contact me", "Send me an email", "mailto:www.yngvilth@gmail.com")}
                 </div>
             </div>
