72d2a64a1b6ecf72703fb95678cf61022b0cfd10
DonNinja <yngvileo@gmail.com>
2023-03-11 18:06:34 +0000

    Fixed a few colour errors

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -10,7 +10,7 @@ const Landing = () => (
         {Title("Ylth")}
         <div className="pageContainer">
             <h1 className="title">Greetings!</h1>
-            <h2><center>Disclaimer: This website has not been finished yet, and some sites have little to no content.</center></h2>
+            <h2 className="disclaimer"><center>Disclaimer: This website has not been finished yet, and some sites have little to no content.</center></h2>
             <div className="landingContent">
                 <p>
                     Welcome to my personal website! I am Yngvi Leó Þráinsson, an Icelander who graduated from Reykjavík University with a BSc. in Computer Science. For more info about me check my <Link to={about}>About Me</Link> page, and my past projects will be found within the <Link to={projects}>Projects</Link> page.
