edb96b111440cc6b1c1ec339852287b1bb10919c
DonNinja <yngvileo@gmail.com>
2023-05-19 16:05:04 +0000

    Moved Links to landing page

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../styles/links.css"
 import { Title } from "./functions";
 
-function Link(title: string, shortDescription: string, link: string, image?: string, imageAlt?: string) {
+export function ContactLink(title: string, shortDescription: string, link: string, image?: string, imageAlt?: string) {
     return (
         <a href={link} target="_blank" className="link" rel="noreferrer">
             <h3>{title}</h3>
@@ -26,10 +26,10 @@ const Links = () => {
             <div className="pageContainer">
                 <h1 className="title">Links</h1>
                 <div className="linksContainer">
-                    {Link("LinkedIn", "My LinkedIn page", "https://www.linkedin.com/in/yngvi-leó-þráinsson-533630239/", "https://cdn-icons-png.flaticon.com/128/174/174857.png", "Linkedin logo")}
-                    {Link("GitHub", "My personal Github page", "https://github.com/DonNinja", "https://cdn-icons-png.flaticon.com/512/733/733553.png", "GitHub logo")}
-                    {Link("Itch.io", "My personal itch.io page", "https://donninja.itch.io")}
-                    {Link("Contact me", "Send me an email", "mailto:www.yngvilth@gmail.com")}
+                    {ContactLink("LinkedIn", "My LinkedIn page", "https://www.linkedin.com/in/yngvi-leó-þráinsson-533630239/", "https://cdn-icons-png.flaticon.com/128/174/174857.png", "Linkedin logo")}
+                    {ContactLink("GitHub", "My personal Github page", "https://github.com/DonNinja", "https://cdn-icons-png.flaticon.com/512/733/733553.png", "GitHub logo")}
+                    {ContactLink("Itch.io", "My personal itch.io page", "https://donninja.itch.io")}
+                    {ContactLink("Contact me", "Send me an email", "mailto:www.yngvilth@gmail.com")}
                 </div>
             </div>
         </>
