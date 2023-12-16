fcb4a194e2a941553f37ab0fda5071997904564d
DonNinja <yngvileo@gmail.com>
2023-12-16 16:24:05 +0000

    Changed the header layout a bit

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -6,6 +6,7 @@ const Game = () => {
     return (
         <>
             This is supposed to be game with ID {ID}
+            <iframe title="My lil' Sleipnir" src="https://itch.io/embed-upload/4923098?color=00ced4" allowFullScreen={true} width="1024" height="596"><a href="https://ylth.itch.io/my-lil-sleipnir">Play My lil' Sleipnir on itch.io</a></iframe>
         </>
     )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
 import "../styles/header.css"
 import { home, about, projects, games } from "../data/routes";
+import Logo from "../images/Logo/logo.png";
 
 const Header = () => {
     return (
         <div id="header">
-            <Link to={home} className="navLink">Home</Link>
-            <Link to={about} className="navLink">About me</Link>
-            <Link to={projects} className="navLink">Projects</Link>
-            <Link to={games} className="navLink">Games</Link>
+            <Link to={about} className="navLink"><p>About me</p></Link>
+            <Link to={home} className="Logo"><img src={Logo} alt="Home" /></Link>
+            <Link to={projects} className="navLink"><p>Projects</p></Link>
+            {/* <Link to={games} className="navLink">Games</Link> */}
         </div>
     );
 }
